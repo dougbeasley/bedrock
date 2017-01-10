9fb029817c033cbfed04541e3ef48e4f76b3d498
Doug Beasley <beasley.doug@gmail.com>
2017-01-10 11:20:26 -0800

    adding in node image

---
+++ templates/substrate/scripts/docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -vx
+
+echo "deb http://http.debian.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list
+
+apt-get update
+apt-get purge "lxc-docker*"
+apt-get purge "docker.io*"
+
+apt-get update
+apt-get -y install apt-transport-https ca-certificates gnupg2
+
+apt-key adv \
+       --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+       --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "deb https://apt.dockerproject.org/repo debian-jessie main"  > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+apt-cache policy docker-engine
+
+apt-get -y install docker-engine
