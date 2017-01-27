93d1e05d64cac85f42ed396a6e2cd73bce41bf8d
Doug Beasley <beasley.doug@gmail.com>
2017-01-26 18:19:46 -0800

    dynamic template

--- bedrock.tf
+++ bedrock.tf
@@ -201,7 +201,7 @@ resource "google_compute_instance" "internal-proxy" {
 
     name = "proxy-${count.index}"
     zone = "${var.region_zone}"
-    tags = ["proxy", "node", "consul-ui"]
+    tags = ["proxy-node", "consul-ui"]
 
     machine_type = "${var.machine_type["internal-proxy"]}"
 
@@ -285,7 +285,39 @@ resource "google_compute_instance" "internal-proxy" {
     }
 }
 
+resource "google_compute_http_health_check" "default" {
+  name                = "tf-www-basic-check"
+  request_path        = "/"
+  check_interval_sec  = 1
+  healthy_threshold   = 1
+  unhealthy_threshold = 10
+  timeout_sec         = 1
+}
 
+resource "google_compute_target_pool" "default" {
+  name          = "tf-www-target-pool"
+  instances     = ["${google_compute_instance.internal-proxy.*.self_link}"]
+  health_checks = ["${google_compute_http_health_check.default.name}"]
+}
+
+resource "google_compute_forwarding_rule" "default" {
+  name       = "tf-www-forwarding-rule"
+  target     = "${google_compute_target_pool.default.self_link}"
+  port_range = "80"
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "tf-www-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["proxy-node"]
+}
 
 resource "google_compute_firewall" "consul_ingress" {
     name = "consul-internal-access"
